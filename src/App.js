77270c0a9a25a61942927b51ae37229486807195
William Spigner <senditomy@gmail.com>
2020-08-19 16:45:34 -0700

    update

--- src/App.js
+++ src/App.js
@@ -1,26 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useRef } from 'react';
 import './App.css';
+import {useSpring, animated, useChain} from 'react-spring';
+
 
 function App() {
+  const text1Ref = useRef();
+  const text2Ref = useRef();
+  const text3Ref = useRef();
+  const imgRef = useRef();
+
+
+  const text1Style = useAnimation(text1Ref);
+  const text2Style = useAnimation(text2Ref);
+  const text3Style = useAnimation(text3Ref);
+
+  const imgStyle = useSpring({
+    to: {
+      opacity: 1
+    },
+    from: {
+      opacity: 0
+    },
+    ref: imgRef 
+  })
+
+  useChain ([text1Ref, text2Ref, text3Ref, imgRef])
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <animated.h1 style={text1Style}>Save</animated.h1>
+      <animated.h1 style={text2Style}>My</animated.h1>
+      <animated.h1 style={text3Style}>Place</animated.h1>
+      <animated.img src="https://imageshack.com/i/f0yWAbBGj" 
+      height={100%""}
+      alt=""
+      style={imgStyle}
+      />
     </div>
   );
+}
+
+const useAnimation = (ref) => {
+  const spring = useSpring({
+    from: {
+        opacity: 0,
+        position: "absolute",
+        transform: 'scale(0.2)'
+    },
+    to: [
+    {
+        opacity: 1, 
+        transform: 'scale(2.2)'
+    },
+    {
+        opacity: 0,
+        transform: 'scale(0.2)'
+    }
+  ],
+    ref: ref
+  });
+  return spring
 }
 
 export default App;
