c8b8d2458545a98c92c171a2a466a3dc5a46740a
Marc S <39275810+P-ano-past@users.noreply.github.com>
2020-09-15 12:38:33 -0700

    bloop

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+// const favicon = require('express-favicon');
+const path = require('path');
+const port = process.env.PORT || 8080;
+const app = express();
+// app.use(favicon(__dirname + '/build/favicon.ico'));
+// the __dirname is the current directory from where the script is running
+app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, 'build')));
+app.get('/ping', function (req, res) {
+ return res.send('pong');
+});
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+});
+
+app.listen(port);
+console.log('listening @: localhost:' + port)
+
+
+//add: add to start script in package.json
+    // "start": "node server.js",
+    // "start": "react-scripts start",
+
+
+    //took out 'build' lin 14
