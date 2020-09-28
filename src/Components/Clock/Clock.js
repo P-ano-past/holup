8c80ca90f1486c6f7b42b93ec1acf6aede3b3ae6
Marc S <39275810+P-ano-past@users.noreply.github.com>
2020-09-28 13:59:14 -0700

    bloop

--- src/Components/Clock/Clock.js
+++ src/Components/Clock/Clock.js
@@ -1,35 +1,37 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 export default class Clock extends Component {
   constructor(props) {
     super(props);
-    this.state = {date: new Date()};
+    this.state = { date: new Date() };
   }
-  
+
   componentDidMount() {
-    this.timerID = setInterval(
-      () => this.tick(),
-      1000
-    );
+    this.timerID = setInterval(() => this.tick(), 1000);
   }
-  
+
   componentWillUnmount() {
     clearInterval(this.timerID);
   }
-  
+
   tick() {
     this.setState({
-      date: new Date()
+      date: new Date(),
     });
   }
-  
+
   render() {
     return (
-        <p className="clock">{this.state.date.toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'})}</p>
+      <p className="clock">
+        {this.state.date.toLocaleTimeString([], {
+          hour: "2-digit",
+          minute: "2-digit",
+        })}
+      </p>
     );
   }
 }
 
 // need to configure dynamic set interval
-// need to configure date and time seperately. 
-// 
+// need to configure date and time seperately.
+//
--- src/Components/Home/Jumbotron/Jumbo.js
+++ src/Components/Home/Jumbotron/Jumbo.js
@@ -1,214 +1,223 @@
-import React, { Component } from 'react';
-import './Jumbo.css'
-import { Jumbotron, Button, InputGroup, FormControl, Modal, Row, Col, Form } from 'react-bootstrap';
-import './Jumbo.css';
-import Clock from '../../Clock/Clock';
-import axios from 'axios';
+import React, { Component } from "react";
+import "./Jumbo.css";
+import {
+  Jumbotron,
+  Button,
+  InputGroup,
+  FormControl,
+  Modal,
+  Row,
+  Col,
+  Form,
+} from "react-bootstrap";
+import "./Jumbo.css";
+import Clock from "../../Clock/Clock";
+import axios from "axios";
 
 const PORT = process.env.PORT || 3001;
 
 const validEmailRegex = RegExp(
-    // eslint-disable-next-line
-    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+  // eslint-disable-next-line
+  /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
 );
 
 const validPhoneRegex = RegExp(
-    /^\s*(?:\+?(\d{1,3}))?[-. (]*(\d{3})[-. )]*(\d{3})[-. ]*(\d{4})(?: *x(\d+))?\s*$/
-)
-const validateForm = errors => {
-    let valid = true;
-    Object.values(errors).forEach(val => val.length > 0 && (valid = false));
-    return valid;
+  /^\s*(?:\+?(\d{1,3}))?[-. (]*(\d{3})[-. )]*(\d{3})[-. ]*(\d{4})(?: *x(\d+))?\s*$/
+);
+const validateForm = (errors) => {
+  let valid = true;
+  Object.values(errors).forEach((val) => val.length > 0 && (valid = false));
+  return valid;
 };
 
 export default class Jumbo extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      firstName: "",
+      lastName: "",
+      email: "",
+      phone: "",
+      // time: '',
+      errors: {
+        firstName: "",
+        lastName: "",
+        email: "",
+        phone: "",
+      },
+    };
+    this.wrapper = React.createRef();
+  }
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            firstName: '',
-            lastName: '',
-            email: '',
-            phone: '',
-            // time: '',
-            errors: {
-                firstName: '',
-                lastName: '',
-                email: '',
-                phone: '',
-            }
-        };
-        this.wrapper = React.createRef();
-    }
+  handleChange = (event) => {
+    event.preventDefault();
+    const { name, value } = event.target;
+    let errors = this.state.errors;
 
-    handleChange = (event) => {
-        event.preventDefault();
-        const { name, value } = event.target;
-        let errors = this.state.errors;
-    
-        switch (name) {
-          case 'firstName': 
-            errors.firstName = 
-              value.length < 3
-                ? 'First name must be at least 3 characters long!'
-                : '';
-            break;
-          case 'lastName': 
-            errors.lastName = 
-              value.length < 3
-                ? 'Last name must be at least 3 characters long!'
-                : '';
-            break;
-          case 'email': 
-            errors.email = 
-              validEmailRegex.test(value)
-                ? ''
-                : 'Email is not valid!';
-            break;
-          case 'phone': 
-            errors.phone = 
-              validPhoneRegex.test(value)
-                ? ''
-                : 'Phone number is not valid!';
-            break;
-          default:
-            break;
-        }
-        this.setState({errors, [name]: value});
+    switch (name) {
+      case "firstName":
+        errors.firstName =
+          value.length < 3
+            ? "First name must be at least 3 characters long!"
+            : "";
+        break;
+      case "lastName":
+        errors.lastName =
+          value.length < 3
+            ? "Last name must be at least 3 characters long!"
+            : "";
+        break;
+      case "email":
+        errors.email = validEmailRegex.test(value) ? "" : "Email is not valid!";
+        break;
+      case "phone":
+        errors.phone = validPhoneRegex.test(value)
+          ? ""
+          : "Phone number is not valid!";
+        break;
+      default:
+        break;
     }
-    
-    handleSubmit = (event) => {
-        event.preventDefault();
-        console.log(this.state)
-        if(validateForm(this.state.errors)) {
-            axios.post('http://localhost:'+ PORT + '/api/user', { 
-                firstName: this.state.firstName,
-                lastName: this.state.lastName,
-                email: this.state.email,
-                phone: this.state.phone
-            })
-            console.log(PORT)
+    this.setState({ errors, [name]: value });
+  };
 
-            this.setState({
-                firstName: '',
-                lastName: '',
-                email: '',
-                phone: '',
-            })
-        } else {
-            console.error('Invalid Form')
-            this.handleShow()
-        }
-    }
-    handleShow = (event) => {
-        this.setState({show: true})
-    }
+  handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(this.state);
+    if (validateForm(this.state.errors)) {
+      axios.post("http://localhost:" + PORT + "/api/user", {
+        firstName: this.state.firstName,
+        lastName: this.state.lastName,
+        email: this.state.email,
+        phone: this.state.phone,
+      });
+      console.log(PORT);
 
-    handleClose = (event) => {
-        this.setState({show: false})
+      this.setState({
+        firstName: "",
+        lastName: "",
+        email: "",
+        phone: "",
+      });
+    } else {
+      console.error("Invalid Form");
+      this.handleShow();
     }
+  };
+  handleShow = (event) => {
+    this.setState({ show: true });
+  };
 
-    render() {
-        const {errors} = this.state; 
-        return (
-            <Jumbotron>
-                <h1 className="display-4">Holup</h1>
-                <hr className="my-4"/>
-                <p className="lead">Please feel free to check-in and you will be notified once you are ready.</p>
-                <h2 className="time">Current time: <Clock /></h2>
-                <Button onClick={this.handleShow} >Check-In</Button>
-                <Modal 
-                    className="custom_modal" 
-                    show={this.state.show} 
-                    onHide={this.handleClose}
-                    ref={this.wrapper}
-                    >
-                        {this.props.children}
-                        <Modal.Header closeButton={this.handleClose}>
-                            <Modal.Title>Check-In here!</Modal.Title>
-                        </Modal.Header>
-                        <Row>
-                            <Col>
-                            <Form onSubmit={this.handleSubmit} noValidate>
-                                <br/>
-                                <InputGroup size="lg">
-                                    <FormControl 
-                                    placeholder="First name:"
-                                    type="text"
-                                    name='firstName'
-                                    aria-label="Large" 
-                                    aria-describedby="inputGroup-sizing-sm"
-                                    value={this.state.firstName} 
-                                    onChange={this.handleChange}
-                                    noValidate
-                                    />
-                                    {
-                                    errors.firstName.length > 0 &&
-                                    <span className="error">{errors.firstName}</span>
-                                    }
-                                </InputGroup>
-                                <br/>
-                                 <InputGroup size="lg">
-                                    <FormControl 
-                                    placeholder="Last name:"
-                                    type="text"
-                                    name="lastName"
-                                    aria-label="Large" 
-                                    aria-describedby="inputGroup-sizing-sm"
-                                    value={this.state.lastName} 
-                                    onChange={this.handleChange}
-                                    required
-                                    />
-                                    {
-                                    errors.lastName.length > 0 &&
-                                    <span className="error">{errors.lastName}</span>
-                                    }
-                                </InputGroup>
-                                <br/>
-                                <InputGroup size="lg">
-                                    <FormControl 
-                                    placeholder="Email:"
-                                    type="email"
-                                    name="email"
-                                    aria-label="Large" 
-                                    aria-describedby="inputGroup-sizing-sm"
-                                    value={this.state.email} 
-                                    onChange={this.handleChange}
-                                    pattern=".+@globex.com"
-                                    required
-                                    />
-                                    {
-                                    errors.email.length > 0 && 
-                                    <span className='error'>{errors.email}</span>
-                                    }
-                                </InputGroup>
-                                <br/>
-                                <InputGroup size="lg">
-                                    <FormControl 
-                                    placeholder="Phone:"
-                                    type="tel"
-                                    name="phone"
-                                    aria-label="Large" 
-                                    aria-describedby="inputGroup-sizing-sm"
-                                    value={this.state.phone} 
-                                    onChange={this.handleChange}
-                                    pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}"
-                                    required
-                                    />
-                                    {errors.phone.length > 0 && 
-                                    <span className='error'>{errors.phone}</span>}
-                                </InputGroup>
-                                <br/>
-                                <Modal.Footer>
-                                    <Button type="submit" onClick={ this.handleClose }>Submit</Button>
-                                </Modal.Footer>
-                            </Form>
-                            </Col>
-                        </Row>
-                    </Modal>
-            </Jumbotron>
-        )
-    }
+  handleClose = (event) => {
+    this.setState({ show: false });
+  };
+
+  render() {
+    const { errors } = this.state;
+    return (
+      <Jumbotron>
+        <h1 className="display-4">Holup</h1>
+        <hr className="my-4" />
+        <p className="lead">
+          Please feel free to check-in and you will be notified once you are
+          ready.
+        </p>
+        <h2 className="time">
+          Current time: <Clock />
+        </h2>
+        <Button onClick={this.handleShow}>Check-In</Button>
+        <Modal
+          className="custom_modal"
+          show={this.state.show}
+          onHide={this.handleClose}
+          ref={this.wrapper}
+        >
+          {this.props.children}
+          <Modal.Header closeButton={this.handleClose}>
+            <Modal.Title>Check-In here!</Modal.Title>
+          </Modal.Header>
+          <Row>
+            <Col>
+              <Form onSubmit={this.handleSubmit} noValidate>
+                <br />
+                <InputGroup size="lg">
+                  <FormControl
+                    placeholder="First name:"
+                    type="text"
+                    name="firstName"
+                    aria-label="Large"
+                    aria-describedby="inputGroup-sizing-sm"
+                    value={this.state.firstName}
+                    onChange={this.handleChange}
+                    noValidate
+                  />
+                  {errors.firstName.length > 0 && (
+                    <span className="error">{errors.firstName}</span>
+                  )}
+                </InputGroup>
+                <br />
+                <InputGroup size="lg">
+                  <FormControl
+                    placeholder="Last name:"
+                    type="text"
+                    name="lastName"
+                    aria-label="Large"
+                    aria-describedby="inputGroup-sizing-sm"
+                    value={this.state.lastName}
+                    onChange={this.handleChange}
+                    required
+                  />
+                  {errors.lastName.length > 0 && (
+                    <span className="error">{errors.lastName}</span>
+                  )}
+                </InputGroup>
+                <br />
+                <InputGroup size="lg">
+                  <FormControl
+                    placeholder="Email:"
+                    type="email"
+                    name="email"
+                    aria-label="Large"
+                    aria-describedby="inputGroup-sizing-sm"
+                    value={this.state.email}
+                    onChange={this.handleChange}
+                    pattern=".+@globex.com"
+                    required
+                  />
+                  {errors.email.length > 0 && (
+                    <span className="error">{errors.email}</span>
+                  )}
+                </InputGroup>
+                <br />
+                <InputGroup size="lg">
+                  <FormControl
+                    placeholder="Phone:"
+                    type="tel"
+                    name="phone"
+                    aria-label="Large"
+                    aria-describedby="inputGroup-sizing-sm"
+                    value={this.state.phone}
+                    onChange={this.handleChange}
+                    pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}"
+                    required
+                  />
+                  {errors.phone.length > 0 && (
+                    <span className="error">{errors.phone}</span>
+                  )}
+                </InputGroup>
+                <br />
+                <Modal.Footer>
+                  <Button type="submit" onClick={this.handleClose}>
+                    Submit
+                  </Button>
+                </Modal.Footer>
+              </Form>
+            </Col>
+          </Row>
+        </Modal>
+      </Jumbotron>
+    );
+  }
 }
 
-// auto focus on first name, enter button into next section. 
+// auto focus on first name, enter button into next section.
